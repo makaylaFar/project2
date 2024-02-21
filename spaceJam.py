d2fec78b7005aad4623675f658cbecb99a1150d0
makaylaFar <159464385+makaylaFar@users.noreply.github.com>
2024-02-20 23:57:51 -0500

    oop

--- spaceJam.py
+++ spaceJam.py
@@ -7,75 +7,37 @@ import spaceJamClasses as spaceJamClasses
 class spaceJam(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
-        def DrawCloudDefense(planet, nickName, centralObject):
-            unitVec = defensePaths.Cloud()
-            unitVec.normalize()
-            spaceJamClasses.Drone(self.loader, "./assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./assets/DroneDefender/octotoad1_auv.png", position, 10)
+
+    def DrawCloudDefense(self, centralObject, droneName):
+        unitVec = defensePaths.Cloud()
+        unitVec.normalize()
+        position = unitVec * 500 + centralObject.modelNode.getPos()
+        spaceJamClasses.Drone(self.loader, "./assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./assets/DroneDefender/octotoad1_auv.png", position, 10)
+
+    def DrawBaseballSeams(self, centralObject, droneName, step, numSeams, radius = 1):
+        unitVec = defensePaths.BaseballSeams(step, numSeams, B = 0.4)   
+        unitVec.normalize()
+        position = unitVec * radius * 250 + centralObject.modelNode.getPos()
+        spaceJamClasses.Drone(self.loader, "./assets/DroneDefender/DroneDefender.obj", self.render, droneName, "./assets/DroneDefender/octotoad1_auv.png", position, 5)
+        
 
     def sceneSetup(self):
 
-        self.planet1 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/universe/universe.jpeg", (-6000, -3000, -800), 250)
+        self.universe = spaceJamClasses.universe(self.loader, "./assets/universe/Universe.x", self.render,'universe',"./assets/universe/universe.jpeg", (0, 0, 0), 10000)
+        self.planet1 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/planets/green.jpg", (-6000, -3000, -800), 250)
+        self.planet2 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/planets/purple.jpg", (0, 6000, 0), 300)
+        self.planet3 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/planets/rocky.jpg", (-6000, -5000, 200), 500)
+        self.planet4 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/planets/sandy.jpg", (300, 6000, 500), 150)
+        self.planet5 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/planets/mars.jpg", (700, 2000, 100), 500)
+        self.planet6 = spaceJamClasses.Planet(self.loader, "./assets/planets/protoPlanet.x", self.render,'planet1',"./assets/planets/sun.jpg", (0, -900, -1400), 700)
+
+        fullCycle = 60
 
         for j in range(fullCycle):
             spaceJamClasses.Drone.droneCount += 1
             nickName = "Drone" + str(spaceJamClasses.Drone.droneCount)
-            self.DrawCloudDefense(self.planet1, nickName, centralObject)
-
-
-        #self.universe = self.loader.loadModel("./assets/universe/Universe.x")
-        #self.universe.reparentTo(self.render)
-        #self.universe.setScale(15000)
-
-        #tex = self.loader.loadTexture("")
-        #self.universe.setTexture(tex, 1)
-
-        #self.planet1 = self.loader.loadModel("")
-        #self.planet1.reparentTo(self.render)
-        #self.planet1.setPos(1000, -3000, 67)
-        #self.planet1.setScale(350)
-
-        #tex2 = self.loader.loadTexture("./assets/planets/green.jpg")
-        #self.planet1.setTexture(tex2, 1)
-
-       # self.planet2 = self.loader.loadModel("./assets/planets/protoPlanet.x")
-     #   self.planet2.reparentTo(self.render)
-     #   self.planet2.setPos(1000, 4000, 67)
-     #   self.planet2.setScale(350)
-
-     #   tex3 = self.loader.loadTexture("./assets/planets/purple.jpg")
-       # self.planet2.setTexture(tex3, 1)
-
-     #   self.planet3 = self.loader.loadModel("./assets/planets/protoPlanet.x")
-    #    self.planet3.reparentTo(self.render)
-     #   self.planet3.setPos(3000, 5000, 67)
-     #   self.planet3.setScale(350)
-#
-      #  tex4 = self.loader.loadTexture("./assets/planets/rocky.jpg")
-      #  self.planet3.setTexture(tex4, 1)
-#
-      #  self.planet4 = self.loader.loadModel("./assets/planets/protoPlanet.x")
-      #  self.planet4.reparentTo(self.render)
-      #  self.planet4.setPos(2000, 1000, 67)
-      #  self.planet4.setScale(350)
-
-       # tex5 = self.loader.loadTexture("./assets/planets/sandy.jpg")
-       # self.planet4.setTexture(tex5, 1)
-
-       # self.planet5 = self.loader.loadModel("./assets/planets/protoPlanet.x")
-      #  self.planet5.reparentTo(self.render)
-      #  self.planet5.setPos(1000, 3000, 67)
-       # self.planet5.setScale(350)
-
-       # tex6 = self.loader.loadTexture("./assets/planets/mars.jpg")
-       # self.planet5.setTexture(tex6, 1)
-
-       # self.planet6 = self.loader.loadModel("./assets/planets/protoPlanet.x")
-        #self.planet6.reparentTo(self.render)
-        #self.planet6.setPos(-500, 5600, 67)
-       # self.planet6.setScale(350)
-
-       # tex7 = self.loader.loadTexture("./assets/planets/sun.jpg")
-       # self.planet6.setTexture(tex7, 1)
+            self.DrawCloudDefense(self.planet1, nickName)
+            self.DrawBaseballSeams(self.planet2, nickName, 1, 3)
 
         #self.ship = self.loader.loadModel("./assets/spaceShip/Khan.x")
        # self.ship.reparentTo(self.render)
@@ -92,7 +54,7 @@ class spaceJam(ShowBase):
 
         #tex9 = self.loader.loadTexture("./assets/spaceStation/SpaceStation1_Dif2.png")
        # self.spaceStation.setTexture(tex9, 1)
-    fullCycle = 60
+    
 
 
 
--- spaceJamClasses.py
+++ spaceJamClasses.py
@@ -1,5 +1,6 @@
 from panda3d.core import Vec3
-
+from panda3d.core import *
+from direct.showbase.ShowBase import ShowBase
 class Planet(ShowBase):
     def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
         self.modelNode = loader.loadModel(modelPath)
@@ -12,6 +13,17 @@ class Planet(ShowBase):
         self.modelNode.setTexture(tex, 1)
     
 class Planet(ShowBase):
+    def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
+        self.modelNode = loader.loadModel(modelPath)
+        self.modelNode.reparentTo(parentNode)
+        self.modelNode.setPos(posVec)
+        self.modelNode.setScale(scaleVec)
+
+        self.modelNode.setName(nodeName)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+
+class Drone(ShowBase):
     droneCount = 0
     def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
         self.modelNode = loader.loadModel(modelPath)
@@ -23,7 +35,7 @@ class Planet(ShowBase):
         tex = loader.loadTexture(texPath)
         self.modelNode.setTexture(tex, 1)
 
-class droneDefender(ShowBase):
+class universe(ShowBase):
     def __init__(self, loader: Loader, modelPath: str, parentNode: NodePath, nodeName: str, texPath: str, posVec: Vec3, scaleVec: float):
         self.modelNode = loader.loadModel(modelPath)
         self.modelNode.reparentTo(parentNode)
